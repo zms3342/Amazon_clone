c82f081d443c8a1fb13dc0ca4f0b511f8d842262
Zafar <zms3342@gmail.com>
2020-07-17 13:06:00 -0400

    added banner image and applied a gradient to it

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,11 @@ import "./Home.css"
 function Home(){
 	return(
 		<div className="home">
+			{/* can we make this a carosel */}
+			<img 
+			className="home__image"
+			src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/NWViNTVmMjkt/NWViNTVmMjkt-N2FhNWE4ZjYt-w3000._CB433824727_.jpg"
+			alt="" />
 		</div>
 		);
 }
